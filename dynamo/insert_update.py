1c17c514402fc7f80e6ad65285313d91ef2b5b54
Huma Zafar <zafar.huma@gmail.com>
2019-02-23 16:03:48 -0500

    Implement remaining dynamo functions.

--- dynamo/insert_update.py
+++ dynamo/insert_update.py
@@ -96,18 +96,82 @@ def remove_disinterest_paper(date, paperURI, user):
             UpdateExpression="SET #notInterested = :value"
         )
 
-def add_read_paper(date, user, paperURI):
+def add_read_paper(date, paperURI, user):
     """Indicate that user has read / is reading the given paper."""
-    pass
+    paper = diversion_for_week_paper(date, paperURI)
+    readers = paper['HaveRead']
+    if user not in readers:
+        readers.append(user)
+        resp = table.update_item(
+            Key={"WeekOf": date, "Paper": paperURI},
+            ExpressionAttributeNames={
+                "#readers": "HaveRead"
+            },
+            ExpressionAttributeValues={
+                ":value": readers
+            },
+            UpdateExpression="SET #readers = :value"
+        )
 
-def add_rating_paper(date, user, paperURI, rating):
+def add_rating_paper(date, paperURI, user, rating):
     """Add a rating for the given paper by the given user."""
-    pass
+    paper = diversion_for_week_paper(date, paperURI)
+    ratings = paper['Ratings']
+    if (user not in ratings) or (ratings[user] != rating):
+        resp = table.update_item(
+            Key={"WeekOf": date, "Paper": paperURI},
+            ExpressionAttributeNames={
+                "#ratings": "Ratings",
+                "#user": user
+            },
+            ExpressionAttributeValues={
+                ":value": rating
+            },
+            UpdateExpression="SET #ratings.#user = :value"
+        )
 
 def add_availability_week(date, user):
-    """Indiciate that user is available on the given week."""
-    pass
+    """Indicate that user is available on the given week."""
+    diversions = diversion_for_week(date)
+    for diversion in diversions:
+        unavailable = diversion['Unavailable']
+        if user in unavailable:
+            unavailable.remove(user)
+        available = diversion['Available']
+        if user not in available:
+            available.append(user)
+            resp = table.update_item(
+                Key={"WeekOf": date, "Paper": diversion['Paper']},
+                ExpressionAttributeNames={
+                    "#available": "Available",
+                    "#unavailable": "Unavailable"
+                },
+                ExpressionAttributeValues={
+                    ":available": available,
+                    ":unavailable": unavailable
+                },
+                UpdateExpression="SET #available = :available, #unavailable = :unavailable"
+            )
 
 def add_unavailability_week(date, user):
     """Indicate that user is unavailable on the given week."""
-    pass
+    diversions = diversion_for_week(date)
+    for diversion in diversions:
+        available = diversion['Available']
+        if user in available:
+            available.remove(user)
+        unavailable = diversion['Unavailable']
+        if user not in unavailable:
+            unavailable.append(user)
+            resp = table.update_item(
+                Key={"WeekOf": date, "Paper": diversion['Paper']},
+                ExpressionAttributeNames={
+                    "#available": "Available",
+                    "#unavailable": "Unavailable"
+                },
+                ExpressionAttributeValues={
+                    ":available": available,
+                    ":unavailable": unavailable
+                },
+                UpdateExpression="SET #available = :available, #unavailable = :unavailable"
+            )
